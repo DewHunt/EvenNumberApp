77aa96d71561b34e76fe20d7f99a38d64d6bcf1d
s-sabbir <dew.fog1553@gmail.com>
2015-06-03 00:07:57 +0600

    initial Commit for EvenNumberApp

---
+++ ProblemTwoApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemTwoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("All Even Numbers From 1 to 100 are : ");
+            Console.Write("\n\t");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + ", ");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
